e988bfbb921b0b1dbe0f57482824f8037fb86c00
Sugul <139774330+Sugul@users.noreply.github.com>
2023-07-18 09:18:59 +0530

    Add files via upload

---
+++ inventorymanagementsystem/module.py
@@ -0,0 +1,5 @@
+Python 2.7.17 (v2.7.17:c2f86d86e6, Oct 19 2019, 21:01:17) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================= RESTART: C:/Python27/module.py =======================
+Enter your name :
